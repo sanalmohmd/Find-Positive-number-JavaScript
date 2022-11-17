dc2ae632eddabb6e117b1e3e3144983d56331362
Sanal Mohammed P <108720655+sanalmohmd@users.noreply.github.com>
2022-11-17 11:37:23 +0530

    initail commit

---
+++ sample.js
@@ -0,0 +1,17 @@
+var number = parseInt(prompt('enter a number'));
+
+if(number > 0){
+    document.write('number is positive');
+
+
+}
+
+else if(number == 0){
+    document.write('number is zero')
+
+
+}
+
+else{
+    document.write('number is negative')
+}
